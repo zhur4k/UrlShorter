307a401fd246a1e249d0fdbad974dfe2ff408c32
zhur4k <kostya.zhurovich12052@gmail.com>
2024-09-25 19:33:55 +0300

    Create project

---
+++ UrlShorter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UrlShorter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
