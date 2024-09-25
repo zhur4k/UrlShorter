d3a39b77d147ff0c738d1f06d0ef040fd5357c38
zhur4k <19804731205aytsoKxyi>
2024-09-25 22:20:42 +0300

    Realize interfaces.

--- UrlShorter/Repository/Interfaces/IUrlEntryRepository.cs
+++ UrlShorter/Repository/Interfaces/IUrlEntryRepository.cs
@@ -6,12 +6,22 @@ namespace UrlShorter.Repository.Interfaces
     {
         Task<IEnumerable<UrlEntry>> GetAllAsync();
 
+        Task<UrlEntry> GetByLongUrlAsync(string longUrl);
+
+        Task<UrlEntry> GetByShortUrlAsync(string longUrl);
+
         Task<bool> AddAsync(UrlEntry url);
+
+        Task<bool> IsExistByShortUrlAsync(string shortUrl);
+
+        Task<bool> IsExistByLongUrlAsync(string longUrl);
 
         Task<bool> UpdateAsync(UrlEntry url);
 
         Task<bool> DeleteAsync(UrlEntry url);
 
         Task<bool> SaveAsync();
+
+        Task<UrlEntry> GetByIdAsync(int id);
     }
 }
